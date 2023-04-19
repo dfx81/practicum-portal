d7f73021f485cfd62ad81ad4343d73c50fe448b7
dfx81 <danialfitrighazali@gmail.com>
2023-04-19 09:54:56 +0800

    Try to fetch external data and display

--- base/js/main.js
+++ base/js/main.js
@@ -27,4 +27,35 @@ window.onload = () => {
             document.body.classList.remove("no-scroll");
         }
     );
+
+    fetch("https://649f-103-5-183-43.ngrok-free.app/contacts", {mode: "cors"}).then(
+        (res) => {
+            if (!res.ok) {
+                throw new Error(`HTTP error ${res.status}`);
+            }
+
+            return res.json();
+        }
+    ).then(
+        (data) => {
+            let mail = document.querySelector("#email");
+            mail.innerText = data.email;
+            mail.href = `mailto:${data.email}`;
+            
+            document.querySelector("#address").innerText = data.addresses.base;
+            document.querySelector("#office").innerText = data.addresses.office;
+            
+            let cas = document.querySelector("#cas");
+            cas.innerText = data.phone.cas;
+            cas.href = `tel:${data.phone.cas}`;
+
+            let colgis = document.querySelector("#colgis");
+            colgis.innerText = data.phone.colgis;
+            colgis.href = `tel:${data.phone.colgis}`;
+
+            let cob = document.querySelector("#cob");
+            cob.innerText = data.phone.cob;
+            cob.href = `tel:${data.phone.cob}`;
+        }
+    )
 };
